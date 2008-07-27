8278530f0632e638a64fd68615ba227799e1cc4d
RogerTheriault <RogerTheriault@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-27 21:54:36 +0000

    Updated list of contributors
    git-svn-id: https://plugins.svn.wordpress.org/great-real-estate/trunk@56585 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/about.php
+++ admin/about.php
@@ -8,6 +8,7 @@ function greatrealestate_admin_about() {
 <h2>About Great Real Estate</h2>
 <h3 class="tablenav">Great Real Estate Development</h3>
 <p>This WordPress plugin is developed, supported, and updated by <a href="http://www.RogerTheriault.com/agents/">Roger Theriault</a>.</p>
+<p>Translations: Italian by Rodney Tommasi (Split)
 <h3 class="tablenav">Documentation</h3>
 <p>User guide, documentation, and support forums are available at the 
 <a href="http://www.rogertheriault.com/agents/plugins/great-real-estate-plugin/">Great Real Estate plugin page</a>.
